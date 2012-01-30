3741e43f5fad9e171bf02584ec820c1b490e9d27
Candy Button <cbutton9@gmail.com>
2012-01-30 21:35:16 +0000

    Some basic php files, and a directory for Kanika's Java stuff. All files accessible directly from the web should go in "html".

---
+++ html/footer.php
@@ -0,0 +1,8 @@
+<?php
+  //-----------------------------------------------------------------------
+  // Author: Choosine
+  //-----------------------------------------------------------------------
+?>
+<hr>
+  Created by Alice Zheng, Abbi Ward, Kanika Pasricha, Angela Dai, and Candy Button, January 2012.
+</hr>
---
+++ html/index.php
@@ -0,0 +1,22 @@
+<?php
+  //-----------------------------------------------------------------------
+  // Author: Choosine
+  //-----------------------------------------------------------------------
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head><title>Choosine</title></head><!Page Title (i.e. in browser)>
+  <body>
+    <?php
+      require_once "header.php";
+    ?>
+
+	A site that will let you vote on restaurants.
+	Cool!
+
+    <?php
+      require_once "footer.php";
+    ?>
+  </body>
+</html>
