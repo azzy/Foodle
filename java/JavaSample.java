62c72708e8b1e5c55604b2d9da4e5ab7df32a66c
Candy Button <cbutton9@gmail.com>
2012-01-30 21:53:31 +0000

    Sample java classes.

---
+++ java/JavaSample.java
@@ -0,0 +1,9 @@
+/* A sample java class. */
+public class JavaSample {
+    public static void main(String[] args) {
+	System.out.println(getOutput());
+    }
+    public static String getOutput() {
+	return "Some sample string output";
+    }
+}
