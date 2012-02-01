8874bf21f3684fdde1900ce9dd390ea6653da969
Abbi Ward <abigail.ward9@gmail.com>
2012-01-31 20:20:47 -0500

    yelp api stuff

--- html/yelp-api/php/example.php
+++ html/yelp-api/php/example.php
@@ -54,7 +54,8 @@ $response = json_decode($data);
 print_r($response);
 echo "<br><br><br>";
 print "Hiiiiiiiiiiii";
-//$start = strpos($response, "[name]");
+//$start = strpos($response, "[name]", 0);
+print $response->{'name'};
 // int strpos ( string $haystack , mixed $needle [, int $offset = 0 ] )
 // string substr ( string $string , int $start [, int $length ] )
 //print(substr($response, $start, 15); 
