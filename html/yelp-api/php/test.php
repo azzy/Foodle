d2cbe794f2093a669a9f04481353f4c760578089
Abbi Ward <abigail.ward9@gmail.com>
2012-02-01 22:57:50 -0500

    yelp api stuff

--- html/yelp-api/php/test.php
+++ html/yelp-api/php/test.php
@@ -12,27 +12,24 @@
 <form method="post" action="<?php echo $PHP_SELF;?>">
  Search for: <input type="text" name="tag1" /> <br/>
  Location: <input type="text" name="loc" /> <br/>
- Number of Busnesses to Retun: <input type="text" name="numBus" /><br/> 
+ <!--Number of Busnesses to Retun: <input type="text" name="numBus" /><br/> -->
  <input type="submit" value="Submit" /><br/>
 </form>
 
 <?php
 require_once ('lib/OAuth.php');
-//include("authkeys.php");
 include("access.php");
+include("parse.php");
 
 $tag = $_POST["tag1"];
-$numBus=$_POST["numBus"];
+$numBus=0
 $location=$_POST["loc"];
+
+
 echo "<p> You searched for " . $tag . "</p></br>";;
 $tag = str_replace(" ", "", $tag);
 $location=str_replace(" ", "", $location);
 $name = "Witherspoon-grill";
-
-// --------------OAuth Stuff------------------------------------ 
-// From http://non-diligent.com/articles/yelp-apiv2-php-example/
-// Enter the path that the oauth library is in relation to the php file
-
 echo "<br/>";
 //cut paste = ctrl+space ctrl+w ctrl+y
 // copy = esc w 
@@ -45,73 +42,14 @@ echo $unsigned_url . "</br>";
 
 echo "<h1>Take1</h1>";
 
-$dataEx = access($unsigned_url);
-// Handle Yelp response data
-$response = json_decode($dataEx);
-print_r($response);
-/*
-echo "<h1>Take2</h1>";
-
-    // Set your keys here
-    // Token object built using the OAuth library
-    $token = new OAuthToken($token, $token_secret);
-    // Consumer object built using the OAuth library
-    $consumer = new OAuthConsumer($consumer_key, $consumer_secret);
-    // Yelp uses HMAC SHA1 encoding
-    $signature_method = new OAuthSignatureMethod_HMAC_SHA1();
-    // Build OAuth Request using the OAuth PHP library. Uses the consumer and token object created above.
-    $oauthrequest = OAuthRequest::from_consumer_and_token($consumer, $token, 'GET', $unsigned_url);
-    // Sign the request
-    $oauthrequest->sign_request($signature_method, $consumer, $token);
-    // Get the signed URL
-    $signed_url = $oauthrequest->to_url();
-    // Send Yelp API Call
-    $ch = curl_init($signed_url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    $data = curl_exec($ch); // Yelp response
-    curl_close($ch);
+$data = access($unsigned_url);
 // Handle Yelp response data
 $response = json_decode($data);
 print_r($response);
-*/
-
-// ------------------------------------------------------------
-//print_r($response);
-echo "<br>";
-
-function name($response, $ind) {
-    return "" . $response->businesses[$ind]->name;
-}
-function rating($response, $ind){
-  return "" .  $response->businesses[$ind]->rating;
-}
-function location($response, $ind){
-  return "" .  $response->businesses[$ind]->location->city;
-}
-function snippet($response, $ind){
-  return "" .  $response->businesses[$ind]->snippet_text;
-}
-function ratingimg($response, $ind) {
-    return "" . $response->businesses[$ind]->rating_img_url;
-}
-function snippetimg($response, $ind){
-  return "" .  $response->businesses[$ind]->snippet_image_url;
-}
-function tags($response, $ind){
-  return "" .  $response->businesses[$ind]->categories[0][0];
-}
-function ratingimgsm($response, $ind){
-  return "" .  $response->businesses[$ind]->rating_img_url_small;
-}
-
-echo "<br>";
-echo "<br>";
+echo "<br><br><br>";
 ?>
-
     <h1>Search Results</h1>
     <p> Put the (parsed) search results here</p>
-
 <?php
 //print businesses(0)->name;
 echo "<br> Printing out various info here: <br> ";
@@ -127,14 +65,11 @@ echo "<br><img src='" . ratingimg($response, 0)  . "'>Rating Img normal</img><br
 echo "<img src='" . snippetimg($response, 0)  . "'>Snippet</img><br>";
 echo "<img src='" . ratingimgsm($response, 0)  . "'>Small Rating</img><br>";
 echo tags($response, 0);
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
+echo "<br><br><br><br>";
 print_r($response);
 ?>
 
-<p>and you can put more text hereeee yay?</p>
+<p><br/>Now, we can add more html type things down here.</p>
 
 </body>
 </html>
