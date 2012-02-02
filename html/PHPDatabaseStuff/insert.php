9c21b5e4ce62490da48b4f2d682d8c52ef0f6bff
Candy Button <cbutton9@gmail.com>
2012-02-02 17:48:21 +0000

    testing prepared statements

--- html/PHPDatabaseStuff/insert.php
+++ html/PHPDatabaseStuff/insert.php
@@ -11,11 +11,32 @@ echo $last;
 echo $username;
 echo $email;
 
-mysql_connect('localhost',$username,$password);
-mysql_select_db($database) or die( "Unable to select database"); 
+//mysql_connect('localhost',$username,$password);
+//mysql_select_db($database) or die( "Unable to select database"); 
+//$db = new mysqli('localhost', $username, $password, $database);
+try {
+  $db = new PDO('mysql:host=localhost;dbname='.$database, $username, $password);
 
-$query = "INSERT INTO contacts VALUES ('','$poll','$first','$last','$email')";
-mysql_query($query);
+  $query = "INSERT INTO contacts VALUES ('', ?, ?, ?, ?)";
+  //'$poll','$first','$last','$email')";
 
-mysql_close();
+  if ($stmt = $db->prepare($query)) {
+    $stmt->bindParam(1, $poll);
+    $stmt->bindParam(2, $first);
+    $stmt->bindParam(3, $last);
+    $stmt->bindParam(4, $email);
+    $stmt->execute();
+  }
+
+  $db = null;
+  //mysql_query($query);
+  //mysql_close();
+
+} catch (PDOException $e) {
+  print "Error!: " . $e->getMessage() . "<br/>";
+  die();;
+}
+
+
+
 ?> 
