b3a8e2be573865cd79bcc0403cdda05e5a818e3f
Abbi Ward <abigail.ward9@gmail.com>
2012-02-01 23:10:19 -0500

    yelp api stuff

--- html/yelp-api/php/test.php
+++ html/yelp-api/php/test.php
@@ -36,7 +36,9 @@ echo "<br/>";
 //$unsigned_url = "http://api.yelp.com/v2/business/the-waterboy-sacramento/name";
 //$unsigned_url = "http://api.yelp.com/v2/search?term=tacos&location=sf";
 //$unsigned_url = "http://api.yelp.com/v2/search?term=food,".$tag."&location=".$location."&limit=".$numBus."&sort=2&name=".$name."";
-$unsigned_url = "http://api.yelp.com/v2/search?term=food&location=08544&name=witherspoon_grill&category=food,restaurant";
+//$unsigned_url = "http://api.yelp.com/v2/search?term=food&location=08544&name=witherspoon_grill&category=food,restaurant";
+$unsigned_url = "http://api.yelp.com/v2/search?find_desc=Witherspoon+Grill&find_loc=Princeton%2C+NJ%2C+USA";
+
 echo $unsigned_url . "</br>";
 
 
