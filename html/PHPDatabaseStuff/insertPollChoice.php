f4d82ec1b58c9d2adfc1570cdb3bdc6d1ef0471b
Candy Button <cbutton9@gmail.com>
2012-02-03 01:36:17 +0000

    typo fixed

--- html/PHPDatabaseStuff/insertPollChoice.php
+++ html/PHPDatabaseStuff/insertPollChoice.php
@@ -3,7 +3,7 @@ function insertPollChoice($host,$username, $password, $database, $choiceid, $yel
 {
     mysql_connect($host,$username,$password);
     @mysql_select_db($database) or die( "Unable to select database");
-    $query="INSERT INTO PollChoice".$pollid." VALUES ('$choiceid','$yelpdid')";
+    $query="INSERT INTO PollChoice".$pollid." VALUES ('$choiceid','$yelpid')";
     $insert = mysql_query($query);
     mysql_close(); 
     return $insert;
