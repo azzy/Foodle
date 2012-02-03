4620def7aebffe8a397da1bdca9a4dab491ba6a4
Candy Button <cbutton9@gmail.com>
2012-02-03 02:08:19 +0000

    oops, those test values should not have been hardcoded\!

--- html/PHPDatabaseStuff/insertPollChoice.php
+++ html/PHPDatabaseStuff/insertPollChoice.php
@@ -3,7 +3,7 @@ function insertPollChoice($host,$username, $password, $database, $choiceid, $yel
 {
     mysql_connect($host,$username,$password);
     @mysql_select_db($database) or die( "Unable to select database");
-    string $some_table = "PollChoice".$pollid;
+    $some_table = "PollChoice".$pollid;
     $query="INSERT INTO '$some_table' VALUES ('$choiceid','$yelpid')";
     $insert = mysql_query($query);
     mysql_close(); 
