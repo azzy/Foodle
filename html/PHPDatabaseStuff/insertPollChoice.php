e1ae2b1278040f12e4051395dde120a238d48c4d
Candy Button <cbutton9@gmail.com>
2012-02-03 02:03:09 +0000

    kanika code edits

--- html/PHPDatabaseStuff/insertPollChoice.php
+++ html/PHPDatabaseStuff/insertPollChoice.php
@@ -3,7 +3,8 @@ function insertPollChoice($host,$username, $password, $database, $choiceid, $yel
 {
     mysql_connect($host,$username,$password);
     @mysql_select_db($database) or die( "Unable to select database");
-    $query="INSERT INTO PollChoice".$pollid." VALUES ('$choiceid','$yelpid')";
+    string $some_table = "PollChoice".$pollid;
+    $query="INSERT INTO '$some_table' VALUES ('$choiceid','$yelpid')";
     $insert = mysql_query($query);
     mysql_close(); 
     return $insert;
--- html/PHPDatabaseStuff/insertVotes.php
+++ html/PHPDatabaseStuff/insertVotes.php
@@ -3,7 +3,8 @@ function insertVotes($host,$username, $password, $database, $pollid, $userid, $c
 {
     mysql_connect($host,$username,$password);
     @mysql_select_db($database) or die( "Unable to select database");
-    $query="INSERT INTO Votes".$pollid." VALUES ('$userid','$choiceid','$rank')";
+    $some_table = "Votes".$pollid;
+    $query="INSERT INTO '$some_table' VALUES ('$userid','$choiceid','$rank')";
     $insertv = mysql_query($query);
     mysql_close(); 
     return $insertv;
