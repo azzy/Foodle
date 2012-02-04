59e0dcf9a55a9998932f8b92ae2fd40ee4c3a24d
Candy Button <cbutton9@gmail.com>
2012-02-04 23:11:26 +0000

    fixed up getUserInfo.php

--- html/tests/test_newuser.php
+++ html/tests/test_newuser.php
@@ -2,10 +2,12 @@
 
 include_once("functions/newuser.php");
 include_once("functions/newpoll.php");
-$pollid = newPoll("a fun event", "princeton university");
+$pollid = newPoll("a SUPER fun event", "princeton university");
 echo $pollid;
 $key = newUser($pollid, 'a', "cbutton9@gmail.com", "Candy");
 echo $key;
+echo $info = getUserInfo($key);
+echo $info['urlkey'];
 
 //echo getUserInfo($key);
 //updateUserName($key, "Kanika");
