cded9d91ec822e5c3719fd16d6e3a73e00d31b83
Abbi Ward <abigail.ward9@gmail.com>
2012-02-03 21:04:37 -0500

    more sample jquery ajax files

--- html/yelp-api/php/test-ajax.php
+++ html/yelp-api/php/test-ajax.php
@@ -3,6 +3,14 @@
     //$response['id'] = "restaurant id";
     //echo $_POST['id'] . "6";
     //$json = { "name" : "restaurant name", "id" : "restaurant id"};
-    echo "6";
+    
+    //Get Post variables. Name is the same as
+    // what was in the object that was sent in the jquery
+    if (isset($_POST['sendValue'])) {
+        $value = $_POST['sendValue'];
+    } else {
+        $value = "";
+    } 
+    echo json_encode(array("returnValue"=>"This is returned from PHP : ".$value));
     //echo json_encode($response);
 ?>
