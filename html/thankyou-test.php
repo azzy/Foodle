b5b3c8939e55b5c6ff2e298fde8466919c59bf2d
Abbi Ward <abigail.ward9@gmail.com>
2012-02-04 16:24:50 -0500

    thank you testing

---
+++ html/thankyou-test.php
@@ -0,0 +1,17 @@
+<?php
+  include("header.php");
+  $type = $_GET['type'];
+  $userkey = $_GET['userkey'];
+  //echo '</head><body class="review '.$type'">';
+  echo "</head><body class='review ".$type.">";
+?>
+<div id="banner"><a href="./index.php"><img src="./images/choosine.png"/></a></div>
+<div id="wrapper">
+  <div id="container">
+    <div id="content-area">
+      <div class="text">Thank you for using Choosine! Please use the
+	following URL to view your results.</div>
+	<div class="text">321kl4jsdfglkjl1;14235j;</div>
+      </div>
+    
+    <?php include("footer.php"); ?>
