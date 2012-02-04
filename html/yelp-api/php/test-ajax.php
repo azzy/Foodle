6f5a5906f3aa1c68190fcb0d397335e6ab8e9aec
Abbi Ward <abigail.ward9@gmail.com>
2012-02-03 19:01:59 -0500

    jquery ajax stuff

--- html/yelp-api/php/test-ajax.php
+++ html/yelp-api/php/test-ajax.php
@@ -3,6 +3,6 @@
     $response['id'] = "restaurant id";
     //echo $_POST['id'] . "6";
     $json = { "name" : "restaurant name", "id" : "restaurant id"};
-    echo {"id" : $_POST[searchTxt]};
+    echo {"id" : $_POST['data']};
     //echo json_encode($response);
 ?>
