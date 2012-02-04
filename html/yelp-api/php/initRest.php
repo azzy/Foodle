b6a8460ca7328700919d6731124ed07c2498ad53
Abbi Ward <abigail.ward9@gmail.com>
2012-02-04 13:27:28 -0500

    initialize restaurant exp

--- html/yelp-api/php/initRest.php
+++ html/yelp-api/php/initRest.php
@@ -63,17 +63,17 @@ $arr = array("num"=>$num);
 print_r($arr);
 $arr2 = array("foo" => "bar", 12 => true);
 echo $arr2["foo"];
-/*
+
 for ($i = 0; $i < 10; $i++) {
     $arrRest = array("name"=>name($response, $i), "id"=>id($response, $i), "rating"=>rating($response, $i), "ratingimg"=>ratingimg($response, $i), "snippet"=>snippet($response, $i), "categories"=>($response, $i));
     $arr[$i] = $arrRest;
 }
-*/
+
 echo "<br><br>";
 echo "here";
 print_r(json_decode(json_encode($arr)));
-$arr[0] = array("name"=>"testname", "id"=>"testid");
-print_r(json_decode(json_encode($arr)));
+//$arr[0] = array("name"=>"testname", "id"=>"testid");
+//print_r(json_decode(json_encode($arr)));
 
 
 
