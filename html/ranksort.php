6e30e3b9a180ffa5129d7c9e197c45082a0114fb
Alice Zheng <evolutia2001@gmail.com>
2012-02-05 04:21:53 -0500

    I think this will work now! initiating rest nom

--- html/ranksort.php
+++ html/ranksort.php
@@ -5,6 +5,7 @@
 ?>
 <?php
 $type = $_GET['type'];
+$type = "restaurants";
 $userkey = $_GET['userkey'];
 $nominate = FALSE;
 if (array_key_exists('nominate', $_GET)) {
@@ -81,7 +82,7 @@ if (array_key_exists('location', $pollinfo)) {
     <div id="searchstuff">
       <div class="searchtext"><label>Search:</label>
       <input id="searchtxt" />
-      <?php echo '<a href="javascript: search("'.$location.'")"><img id="search" src="./images/search.png" /></a>'; ?>
+      <?php echo "<a href=\"javascript: search('$location')\"><img id=\"search\" src=\"./images/search.png\" /></a>"; ?>
     </div>
       <a href="javascript: addYelpInfo()"><div id="addnew">
     <!--<img src="./images/add.png" />Add To List</div></a>-->
