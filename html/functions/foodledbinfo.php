2fd0a8cfa31d61dda7e3ba202e036f03ee0b5472
Abbi Ward <abigail.ward9@gmail.com>
2012-02-05 10:08:02 -0500

    updated blah to foodle

--- html/functions/foodledbinfo.php
+++ html/functions/foodledbinfo.php
@@ -1,7 +1,10 @@
 <?php
-
+/*
 $username = 'foodle_admin';
 $database = 'foodle';
 $password = 'guru47*speck';
-
+*/
+$username = 'root';
+$database = 'foodle';
+$password = 'H0uCGxcIIzasUS3CMHdX9LfRVXC1Uqz9xL5YU9kNohYKTkg10RaaWFq6DzLlpSr';
 ?>
