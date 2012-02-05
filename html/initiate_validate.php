7c5a315bc0bd49fd62099bb095d91f284fffd270
Candy Button <cbutton9@gmail.com>
2012-02-05 03:50:48 +0000

    works?works?works?

--- html/initiate_validate.php
+++ html/initiate_validate.php
@@ -5,25 +5,18 @@ require_once("functions/newuser.php");
 require_once("functions/newpoll.php");
 
 // Uncomment these to run tests (test data)
-$_POST['dinner'] = "fun dinner";
-$_POST['name'] = "somebody special";
-$_POST['email'] = "kanika1091@gmail.com";
-$_POST['location'] = "someplace";
-echo 'DINNER';
+//$_POST['dinner'] = "fun dinner";
+//$_POST['name'] = "somebody special";
+//$_POST['email'] = "2134m";
+//$_POST['location'] = "Princeton, NJ";
+//echo 'DINNER';
 
-echo "reached";
+//echo "reached";
 
 $isValid = isValid($_POST['email'], $_POST['location']);
 
-// BIGGER TODO: Cache all of this "new poll" and "new user" data, but 
-// don't write it to the real database until the user selects CREATE POLL!
-
-// Possible TODO: create greater security through cookies rather than the 
-// userkey querystring parameter, and maybe keep a "state" value to represent
-// how far through the form progression they've gotten
-
-if ($isValid) {
-  echo "AAAAAAAAAARRRRRGHHHHHH";
+if($isValid) {
+  //echo "AAAAAAAAAARRRRRGHHHHHH";
   // TODO: if the form data is valid, save it to the database
   $pollid = newPoll($_POST['dinner'], $_POST['location']);
   // create an admin user for the poll
@@ -37,3 +30,4 @@ if ($isValid) {
 }
 
 ?>
+
