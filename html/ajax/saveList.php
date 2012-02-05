f187fce0fa5273c5135a15b6441631cfe6379c8a
Candy Button <cbutton9@gmail.com>
2012-02-05 01:08:34 +0000

    now outputting debugging stuff for ranksort to a file\!

--- html/ajax/saveList.php
+++ html/ajax/saveList.php
@@ -6,7 +6,9 @@ include_once("foodledbinfo.php");
 
 $db = new PDO("mysql:host=localhost;dbname={$database}", $username, $password);
 
+ob_start();
 
+// testing stuff only
 $_POST = array();
 $_POST['userkey'] = 'B15F4E6F-29AF-F20C-D2A8-454D941C7230';
 $_POST['0'] = '';
@@ -22,19 +24,18 @@ $stmt = $db->prepare("SELECT * FROM users WHERE urlkey = '$userkey'");
 $stmt->execute();
 $row = $stmt->fetch();
 
-echo "pollid={$row['pollid']}, and ";
+echo var_dump($row);
 
 if ( !array_key_exists('pollid', $row)) {
   echo "User not found.";
-  die();
+  //  die();
 }
 
-echo $pollid = $row['pollid'];
-echo $voterid = $row['userid'];
+$pollid = $row['pollid'];
+$voterid = $row['userid'];
 
 // Delete all old votes by this user
 $stmt = $db->prepare("DELETE FROM votes WHERE pollid = $pollid AND voterid = $voterid");
-//echo "DELETE FROM votes WHERE pollid = $pollid AND voterid = $voterid" . "\n";
 $stmt->execute();
 
 $rank = 1;
@@ -42,7 +43,7 @@ foreach ($_POST as $index => $yelpid) {
   if ($yelpid !== "" and $index !== "userkey") {
     // Get the choiceid for the yelpid
     $query = "SELECT choiceid FROM choices{$pollid} WHERE yelpid = ?";
-    echo "\n{$query}\n";
+    echo($query.$yelpid."\n");
     $stmt = $db->prepare($query);
     $stmt->bindParam(1, $yelpid);
     $stmt->execute();
@@ -58,10 +59,15 @@ foreach ($_POST as $index => $yelpid) {
       $rank += 1;
     }
     else {
-      echo "Bad yelp id: {$yelpid}";
+      echo("Bad yelp id: {$yelpid}");
       // error handle? bad yelp id
     }
   }
 }
+
+$string = ob_get_clean();
+$file = fopen('siterecords', 'a') or die("can't open file");
+fwrite($file, $string);
+fclose($file);
 
 ?>
