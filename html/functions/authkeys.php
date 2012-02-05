6eed4a023c98150ca513a1a09aa0360e5f4368f9
Abbi Ward <abigail.ward9@gmail.com>
2012-02-05 08:33:10 -0500

    new auth keys

--- html/functions/authkeys.php
+++ html/functions/authkeys.php
@@ -6,11 +6,15 @@ $consumer_secret = "2l3wSMGLsYA0cQCgqnMSVUZpSus";
 $token = "i5m-nLRW3o-18T2NTisIgGrzRPRpCWfZ";
 $token_secret = "8YnLHst61xmdcAnIIXo3KYEtWBE";*/
 //Choosine:
-$consumer_key = "T7h8nAcJA5KvfPiroKWooQ";
+/*$consumer_key = "T7h8nAcJA5KvfPiroKWooQ";
 $consumer_secret = "jLVWAmjRGAGQSKw9EJbpKS5GYzw";
 $token = "ZQO9F_0HeL7XcH54Z66F4NLZ7pAUraN1";
-$token_secret = "GZV-bIEVQKt0NkueKnh4tieKlY8";
-
+$token_secret = "GZV-bIEVQKt0NkueKnh4tieKlY8";*/
+//ChoosinetheThird:
+$consumer_key = "o1Ubc61o8INTxQ_5c7mEBQ";
+$consumer_secret = "t53l2RblIYt6Ub603hJB7Xd4lpk";
+$token = "eymlcQgvAQiKexs9ErOzSBeLUURNvROy";
+$token_secret = "GZV-_Mt8raHuU6aL6lmsqFD6E_wb4QY";
 
 
 ?>
