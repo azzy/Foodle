4d2b15e8a66ad50be82be8fec4e79002107deb67
Abbi Ward <abigail.ward9@gmail.com>
2012-02-05 02:40:58 -0500

    vote restuarants intializer

--- html/functions/voteRest-initialize.php
+++ html/functions/voteRest-initialize.php
@@ -1,5 +1,19 @@
 <?php
-
+    require_once ('lib/OAuth.php');
+    include("access.php");
+    include("parse.php");
+    include("formmatch.php");
+    include("foodledbinfo.php");
+    include("newpoll.php");
+    include("newuser.php");
+    
+    $userkey = "B3EF2465-24E8-AC67-5076-0D3C22571FD0";
+    
+    $userinfo = getUserInfo($userkey);
+    $pollid = $userinfo['pollid'];
+    
+    
+    //echo("<br/><br/>");
 ?>
 <html>
     <head>
@@ -8,6 +22,7 @@
 
 <body>
     lalalala
+    <p><?php echo("userkey ".$userkey." pollid ".$pollid."<br/><br/>");?></p>
 </body>
 </html>
 
