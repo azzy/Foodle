5fa93f9293b8e7c819742f67d9688fdadc4feca4
Alice Zheng <evolutia2001@gmail.com>
2012-03-16 00:09:29 -0400

    uploading Kanika's edits (not in proper filenames)

---
+++ html/PHPDatabaseStuff/sendUsersEmail2.php
@@ -0,0 +1,37 @@
+<?php
+function sendEmail($to, $url, $from, $input)
+{ 
+ $subject = "via Choosine: Where should we go eat?";
+ //echo $body = "I've created a poll to pick a dinner location. Please visit $url to submit your preferences" + $input;
+ echo $body = $input;
+ $headers = "From:".$from."\n";
+ $send = mail($to,$subject,$body,$headers);
+ return $send;
+}
+function sendPollEmail($pollid, $type, $userBody)
+{
+include("dbinfo.inc.php");
+//echo "2";
+mysql_connect('localhost',$username,$password);
+@mysql_select_db($database) or die( "Unable to select database");
+$query="SELECT * FROM users WHERE pollid={$pollid}";
+$result=mysql_query($query);
+$num=mysql_numrows($result); 
+mysql_close();
+$i=0;
+// default value
+$from="mailer@choosine.com";
+while ($i < $num) {
+  if(mysql_result($result,$i,"usertype") == 'a') $from=mysql_result($result,$i,"email");
+  ++$i;
+}
+$i=0;
+while ($i < $num) {
+  $to =mysql_result($result,$i,"email");
+  $userkey=mysql_result($result,$i,"urlkey");
+  // TODO: add type into url
+  sendEmail($to, "http://www.choosine.com/ranksort.php?type={$type}&userkey={$userkey}", $from, $userBody);
+  ++$i;
+}
+}
+?>
---
+++ html/email2.php
@@ -0,0 +1,81 @@
+<?php
+  // have to do this stuff before printing headers!!!
+  if (array_key_exists('userkey', $_GET)) {
+    $type = $_GET['type'];
+    $userkey = $_GET['userkey'];
+    $userBody = $_GET['message'];
+  } else {
+    $type = $_POST['type'];
+    $userkey = $_POST['userkey'];
+    $userBody = $_GET['message'];
+  }
+
+/*
+// debug
+ $_POST = array();
+ $_POST['submit'] = 'create poll';
+ $type = $_POST['type'] = 'restaurants';
+ $userkey = $_POST['userkey'] = '9F3EE83E-1011-193F-0902-A9FD2FC8C0FB';
+ $_POST['email1'] = 'kanika1091@gmail.com';
+*/
+  include_once("functions/newuser.php");
+  include_once("functions/newpoll.php");
+  $userinfo = getUserInfo($userkey);
+  $pollid = $userinfo['pollid'];
+
+if (array_key_exists('submit', $_POST) and $_POST['submit'] == 'create poll') {
+    // TODO: some validation
+    $userkeys = array();
+    
+    foreach ($_POST as $field => $useremail) {
+      if ($field !== 'submit' and $field !== 'userkey' and $field !== 'type' and $useremail) {
+        $userkeys[] = newUser($pollid, 'v', $useremail);
+      }
+    }
+    include_once('PHPDatabaseStuff/sendUsersEmail2.php');
+    sendPollEmail($pollid, $type, $userBody); //changed this, as well as added an input type textbox below - kanika
+    header("Location: ./thankyou.php?type={$type}&userkey={$userkey}");
+    exit();
+  }
+include("header.php");
+echo '</head><body class="emails '.$type.'">';
+
+?>
+<div id="banner"><a href="./index.php"><img src="./images/choosine.png"/></a></div>
+<div id="wrapper">
+  <div id="container">
+    <div id="content-area">
+    <div class="text">Your Guests&apos; Emails:</div>
+    <form name="input" method="post" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
+    <div class="form" id="emails-form">
+      <input name="email1"/>
+      <input name="email2"/>
+      <input name="email3"/>
+      <textarea name="message"/> 
+    </div>
+    <input type="hidden" name="userkey" value="<?php echo $userkey; ?>" />
+    <input type="hidden" name="type" value="<?php echo $type; ?>" />
+    <a href="javascript:add_field()"><div id="addnew">
+      <img src="./images/add.png" />Add another person</div></a>
+      <!-- <a href='<?php ?>'> --><input type="submit" value="create poll" name="submit" class="submit" /> <!--</a>-->
+	</form>
+	
+	<div id="template" style="display:none">
+	  <input />
+	</div>
+    </div>
+    
+    <a href='<?php echo "ranksort.php?type=$type&userkey=$userkey"; ?>'><img src="./images/left.png" id="nav-left" /></a>   
+ <?php include('footer.php'); ?>
+
+
+<script type="text/javascript">
+<!--//Add Field to emails-form.
+function add_field()
+{
+    var div1 = document.createElement('div');
+    div1.innerHTML = document.getElementById('template').innerHTML;
+    document.getElementById('emails-form').appendChild(div1);
+}
+//-->
+</script>
