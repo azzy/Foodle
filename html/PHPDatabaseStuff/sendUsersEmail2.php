52e1270f79e2b754374543f5cdfd9fa5342c2c36
Alice Zheng <evolutia2001@gmail.com>
2012-03-16 01:18:02 -0400

    tweaking new email page

--- html/PHPDatabaseStuff/sendUsersEmail2.php
+++ html/PHPDatabaseStuff/sendUsersEmail2.php
@@ -3,7 +3,7 @@ function sendEmail($to, $url, $from, $subject, $input)
 { 
   //$subject = "via Choosine: Where should we go eat?";
  //echo $body = "I've created a poll to pick a dinner location. Please visit $url to submit your preferences" + $input;
- $body = echo $input."\nPlease visit ".$url." to submit your preferences.";
+ $body = $input."\nPlease visit ".$url." to submit your preferences.";
  $headers = "From:".$from."\n";
  $send = mail($to,$subject,$body,$headers);
  return $send;
